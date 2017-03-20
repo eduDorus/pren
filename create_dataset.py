7c7d40d5b29ea4095772aa517a9d12a4d09cd9e7
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2017-03-20 16:56:36 +0100

    Add performance test

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,6 @@
 # Load path/class_id image file:
-dataset_folder = 'pren_dataset'
+data_folder = 'data/pren_dataset_large'
 
 # Build a HDF5 dataset (only required once)
 from tflearn.data_utils import build_hdf5_image_dataset
-build_hdf5_image_dataset(dataset_folder, image_shape=(128, 128), mode='folder', output_path='dataset.h5', categorical_labels=True, normalize=True)
+build_hdf5_image_dataset(data_folder, image_shape=(128, 128), mode='folder', output_path='data/dataset_large.h5', categorical_labels=True, normalize=True, grayscale=True)

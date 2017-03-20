167e99acb351bb184e1a1726df0a35ef472a7bee
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2017-03-20 16:46:54 +0100

    Add python notebook

--- cnn.py
+++ cnn.py
@@ -1,6 +1,5 @@
 from __future__ import division, print_function, absolute_import
 import numpy as np
-import tflearn
 from tflearn.data_utils import shuffle, to_categorical
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
@@ -10,7 +9,7 @@ from tflearn.data_augmentation import ImageAugmentation
 from tflearn.data_utils import image_preloader
 
 # Load path/class_id image file:
-data_folder = 'data/pren_dataset_large'
+data_folder = 'data/pren_dataset_mid'
 
 # Build the preloader array, resize images to 128x128
 X, Y = image_preloader(data_folder, image_shape=(128, 128), mode='folder', categorical_labels=True, normalize=True, grayscale=True, files_extension='.jpg')
@@ -18,7 +17,8 @@ X, Y = image_preloader(data_folder, image_shape=(128, 128), mode='folder', categ
 # Data loading and preprocessing
 from sklearn.model_selection import train_test_split
 
-X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.10, random_state=42)
+X, X_test, Y, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+print(Y[1])
 #X, Y = shuffle(X, Y)
 #Y = to_categorical(Y, 5)
 #Y_test = to_categorical(Y_test, 5)
@@ -41,18 +41,23 @@ img_aug.add_random_rotation(max_angle=25.)
 network = input_data(shape=[None, 128, 128, 1],
                      data_preprocessing=img_prep,
                      data_augmentation=img_aug)
-network = conv_2d(network, 32, 7, strides=3, activation='relu')
-network = max_pool_2d(network, 2)
-network = conv_2d(network, 64, 5, strides=2, activation='relu')
-network = max_pool_2d(network, 2)
-network = conv_2d(network, 128, 3, strides=1, activation='relu')
-network = max_pool_2d(network, 2)
-network = fully_connected(network, 512, activation='relu')
+network = conv_2d(network, 32, 3, strides=1, activation='relu')
+network = max_pool_2d(network, 4)
+network = conv_2d(network, 32, 3, strides=1, activation='relu')
+
+#network = max_pool_2d(network, 4)
+#network = conv_2d(network, 32, 3, strides=1, activation='relu')
+#network = max_pool_2d(network, 4)
+#network = max_pool_2d(network, 2)
+#network = conv_2d(network, 256, 3, strides=1, activation='relu')
+#network = max_pool_2d(network, 2)
+
+network = fully_connected(network, 96, activation='relu')
 network = dropout(network, 0.5)
 network = fully_connected(network, 5, activation='softmax')
 network = regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
-                     learning_rate=0.01)
+                     learning_rate=0.1)
 
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=0)

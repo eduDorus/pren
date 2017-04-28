810330210d3fa268bac8fe9cc43385a6ec0169aa
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2017-04-28 17:37:16 +0200

    Created scripts for testing

---
+++ scripts/capture_ride.py
@@ -0,0 +1,21 @@
+from time import sleep
+from picamera import PiCamera
+
+# Set run id
+FPS = 4
+SLEEP_TIME = 1 / FPS
+RUN_ID = 0
+PATH = 'run/${0}'.format(RUN_ID)
+BOUNDARY = 30 * FPS
+CAPTURE_ID = 0
+
+with PiCamera() as camera:
+    camera.resolution = (320, 240)
+    sleep(2)
+
+    while CAPTURE_ID < BOUNDARY:
+        sleep(SLEEP_TIME)
+        camera.capture('{0}/{1}_{2}.jpg'.format(PATH, RUN_ID, CAPTURE_ID))
+        print(
+            'captured image : {0}/{1}_{2}.jpg'.format(PATH, RUN_ID, CAPTURE_ID))
+        CAPTURE_ID += 1
---
+++ scripts/realtime_predictions.py
@@ -0,0 +1,70 @@
+# Import time for performance measurements
+import numpy as np
+from picamera import PiCamera, array
+from time import time, sleep
+from tflearn import DNN
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from tflearn.data_preprocessing import ImagePreprocessing
+from tflearn.data_augmentation import ImageAugmentation
+
+# Start time measuring
+START_TIME = time()
+
+# Model URL
+MODEL_URL = '../models/final_model_aug_prep_4'
+
+# Real-time data augmentation (This is only used while training the DNN)
+img_aug = ImageAugmentation()
+img_aug.add_random_blur(0.25)
+img_aug.add_random_rotation(max_angle=10.0)
+
+# Real-time data preprocessing
+img_prep = ImagePreprocessing()
+img_prep.add_featurewise_zero_center()
+img_prep.add_featurewise_stdnorm()
+print("--- {0} seconds for image preprocessing/augmentation functions ---".format(round(time() - START_TIME)))
+
+# Convolutional network building
+network = input_data(shape=[None, 120, 160, 3],
+                     data_augmentation=img_aug,
+                     data_preprocessing=img_prep)
+network = conv_2d(network, 64, 5, activation='relu')
+network = max_pool_2d(network, 4)
+network = conv_2d(network, 64, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = conv_2d(network, 128, 3, activation='relu')
+network = max_pool_2d(network, 2)
+network = fully_connected(network, 256, activation='relu')
+network = dropout(network, 1)
+network = fully_connected(network, 5, activation='softmax')
+network = regression(network, optimizer='adam',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+# Create the model
+model = DNN(network, tensorboard_verbose=0)
+print("--- {0} seconds for creating the cnn model ---".format(round(time() - START_TIME)))
+
+# Load the model
+model.load(MODEL_URL)
+print("--- {0} seconds for loading the model ---".format(round(time() - START_TIME)))
+
+# Capture images for prediction
+with PiCamera() as camera:
+    camera.resolution = (160, 120)
+    sleep(2)
+    with array.PiRGBArray(camera) as output:
+        while True:
+            camera.capture(output, 'rgb')
+
+            # Make prediction
+            prediction_time = time()
+            prediction = model.predict(
+                output.array.astype(float).reshape((1, 120, 160, 3)))
+            print(np.round(prediction, 2))
+
+            print(
+                "--- {0} seconds a prediction ---".format(time() - prediction_time))
+            output.truncate(0)

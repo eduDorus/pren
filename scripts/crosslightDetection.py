d664c929d0b39352f5f11f008506b7bc0510b4dc
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2017-07-05 05:45:17 +0200

    Update Script

--- scripts/crosslightDetection.py
+++ scripts/crosslightDetection.py
@@ -40,31 +40,33 @@ def loadCrosslightModel(image_height, image_width):
 
 def loadCifferModel(image_height, image_width):
 
+    #256
+
     model = Sequential()
-    model.add(Conv2D(16, (7, 7), activation='relu', padding='same', name='block1_conv1', input_shape=(image_height, image_width, 1)))
-    model.add(Conv2D(16, (7, 7), activation='relu', padding='same', name='block1_conv2',))
-    model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='block1_pool'))
+    model.add(Conv2D(16, (7, 7), activation='relu', padding='same', name='block1_conv1', input_shape=(image_height, image_width, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block1_pool'))
 
-    model.add(Conv2D(32, (5, 5), activation='relu', padding='same', name='block2_conv1',))
-    model.add(Conv2D(32, (5, 5), activation='relu', padding='same', name='block2_conv2',))
-    model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='block2_pool'))
+    model.add(Conv2D(16, (5, 5), activation='relu', padding='same', name='block4_conv1'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block4_pool'))
 
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block3_conv1',))
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block3_conv2',))
-    model.add(MaxPooling2D(pool_size=(4, 4), strides=(4, 4), name='block3_pool'))
+    model.add(Conv2D(32, (5, 5), activation='relu', padding='same', name='block2_conv1'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block2_pool'))
 
-    # the model so far outputs 3D feature maps (height, width, features)
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', name='block3_conv1'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block3_pool'))
 
-    # this converts our 3D feature maps to 1D feature vectors
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same', name='block5_conv1'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='block5_pool'))
+
     model.add(Flatten())
-    model.add(Dense(128, activation='relu', name="fc-1"))
-    model.add(Dropout(0.8))
-    model.add(Dense(128, activation='relu', name="fc-2"))
-    model.add(Dense(6, activation='sigmoid'))
+    model.add(Dense(128, activation='relu', name="fc-1-1"))
+    model.add(Dropout(0.5))
+    model.add(Dense(128, activation='relu', name="fc-2-1"))
+    model.add(Dense(6, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+                optimizer='rmsprop',
+                metrics=['accuracy'])
 
     model.load_weights('/home/pi/repositories/pren/models/ciffer.h5')
     
@@ -74,13 +76,15 @@ def loadCifferModel(image_height, image_width):
 def crosslightDetection(image_height, image_width, model):
     with PiCamera() as camera:
         camera.resolution = (image_height, image_width)
+        camera.framerate = 3
         sleep(1)
+        
         start_time = 0
         red_counter = 0
 
         with array.PiRGBArray(camera) as output:
             while True:
-                camera.capture(output, 'rgb')
+                camera.capture(output, 'rgb', use_video_port=True)
 
                 x = output.array
                 x = x.reshape((1,) + x.shape)
@@ -96,7 +100,7 @@ def crosslightDetection(image_height, image_width, model):
                     display.zero()
                     start_time = time()
 
-                if ((time() - start_time) > 60):
+                if time() - start_time > 15 and red_counter > 5:
                     print("0")
                     sys.stdout.flush()
                     break
@@ -109,78 +113,94 @@ def crosslightDetection(image_height, image_width, model):
                 output.truncate(0)
 
 
-def cifferDetection(image_height, image_width, model):
+def cifferDetection(image_height, image_width, image_queue):
+
+    start_time = time()
     
     with PiCamera() as camera:
-        with array.PiRGBArray(camera) as stream:
 
-            # Camera Settings
-            camera.resolution = (image_height, image_width)
-            camera.framerate = 15
-            camera.exposure_mode = 'sports'
-            #camera.awb_mode = 'fluorescent
+        camera.resolution = (image_height, image_width)
+        camera.framerate = 5
+        camera.exposure_mode = 'sports'
+
+        with array.PiRGBArray(camera) as stream:
 
             # Warum Up for Camera'
             sleep(0.5)
 
             while True:
+                capture_time = time()
                 camera.capture(stream, 'rgb', use_video_port=True)
 
                 # Process array for CNN prediction
                 x = stream.array
-                x = np.dot(x[...,:3], [.3, .6, .1])
-
-                # Set batchsize to 1 for single prediction
                 x = x.reshape((1,) + x.shape)
-
-                # Set color channel to 1 for grayscale image
-                x = x.reshape(x.shape + (1,))
                 x = x * (1. / 255)
 
-                prediction = np.around(model.predict(x), 2)[0]
+                image_queue.append(x)
+
                 stream.truncate(0)
 
-                if prediction[0] > 0.90:
-                    display.one()
-                    print("1")
-                    sys.stdout.flush()
+                if time() - start_time > 38:
                     break
+                
 
-                if prediction[1] > 0.90:
-                    display.two()
-                    print("2")
-                    sys.stdout.flush()
-                    break
 
-                if prediction[2] > 0.90:
-                    display.tree()
-                    print("3")
-                    sys.stdout.flush()
-                    break
+def predictCiffer(image_queue, model):
+    while True:
 
-                if prediction[3] > 0.90:
-                    display.four()
-                    print("4")
-                    sys.stdout.flush()
-                    break
+        if not len(image_queue):
+            display.three()
+            print("3")
+            sys.stdout.flush()
+            break
 
-                if prediction[4] > 0.90:
-                    display.five()
-                    print("5")
-                    sys.stdout.flush()
-                    break   
+        x = image_queue.pop()
+        prediction = np.around(model.predict(x), 2)[0]
+        #print(prediction)
 
+        if prediction[0] > 0.95:
+            display.one()
+            print("1")
+            sys.stdout.flush()
+            break
+
+        if prediction[1] > 0.95:
+            display.two()
+            print("2")
+            sys.stdout.flush()
+            break
+
+        if prediction[2] > 0.95:
+            display.three()
+            print("3")
+            sys.stdout.flush()
+            break
+
+        if prediction[3] > 0.95:
+            display.four()
+            print("4")
+            sys.stdout.flush()
+            break
+
+        if prediction[4] > 0.95:
+            display.five()
+            print("5")
+            sys.stdout.flush()
+            break
 
 def logic():
 
     # Load Models
     crosslightModel = loadCrosslightModel(128, 128)
-    cifferModel = loadCifferModel(256, 256)
+    cifferModel = loadCifferModel(128, 128)
 
     # Detect Crosslight
     crosslightDetection(128, 128, crosslightModel)
 
     # predict Ciffer
-    cifferDetection(256, 256, cifferModel)
+    image_queue = []
+    cifferDetection(128, 128, image_queue)
+    predictCiffer(image_queue, cifferModel)
 
 logic()
